11bc52e55b94b334ece561c8e66f0998dc3cd3d3
Vladimir Laptop <shandy@shandy-dev.ru>
2023-04-10 17:58:44 +0300

    Remade request for GPT

--- responder.py
+++ responder.py
@@ -7,13 +7,16 @@ class Responder(commands.GroupCog):
         self.bot = bot
         self.responders = [
             386549311837700109,
-        ]
+            714811357169451029,
+        ]  # TODO: move to database
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.author.id not in self.responders:
             return
+        elif message.content is "":
+            return
         async with message.channel.typing():
-            text = f"Придумай шутку про {message.author.name}. Язык русский. Длина до 2000 символов. Его последнее сообщение: \"{message.content}\"."
+            text = f"Генерация шутки про человека {message.author.name} на основе его последнего сообщения \"{message.content}\". Язык русский, максимальная длина сообщения 2000 символов. В ответе должна быть только шутка."
             response = await self.bot.cogs["GPT"].gpt_invoke(text, "text-davinci-003")
             await message.reply(response[1][:2000])
