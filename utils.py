e3b6a432fa452a1471d28763b4fb99b59e677a2e
Vladimir <shandy@shandy-dev.ru>
2023-11-24 14:05:55 +0300

    Added function to convert a string to the desired plural form

--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
 import asyncio
-import json
+from typing import Tuple, Literal
 
 from custom_dataclasses import Track
 from ytdl import ytdl
+
+WORDS = {
+    "years": ["год", "года", "лет"],
+    "months": ["месяц", "месяца", "месяцев"],
+    "weeks": ["неделя", "недели", "недель"],
+    "days": ["день", "дня", "дней"],
+    "hours": ["час", "часа", "часов"],
+    "minutes": ["минута", "минуты", "минут"],
+    "seconds": ["секунда", "секунды", "секунд"]
+}
 
 
 async def get_info_yt(url: str) -> Track:
@@ -17,3 +27,30 @@ async def get_info_yt(url: str) -> Track:
         original_url=url
     )
     return track
+
+
+def convert_word_from_number(
+        word: Literal['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds'],
+        number: int,
+        answer_type: bool = False
+) -> str | Tuple[str, int]:
+    """
+    Преобразует слово от числа в нужное множественное число.
+
+    :param word: Слово
+    :param number: Число
+    :param answer_type: Тип ответа. True - кортеж из строки и числа, False - строка
+
+    :return: Кортеж из строки и числа или строка
+    """
+    remainder = number % 10
+    if remainder == 1:
+        tr_word = WORDS[word][0]
+    elif 2 <= remainder <= 4:
+        tr_word = WORDS[word][1]
+    else:
+        tr_word = WORDS[word][2]
+    if answer_type:
+        return (tr_word, number,)
+    else:
+        return tr_word
