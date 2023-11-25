21aa0d926f8c2f5de3862daac4fdb2f09c4582e0
Vladimir <shandy@shandy-dev.ru>
2023-11-25 23:51:41 +0300

    Added View and Button for searching and selecting a track

--- views.py
+++ views.py
@@ -1,14 +1,12 @@
 import asyncio
 import io
-from typing import List, Any
+from typing import Any, List
 
 import discord.ui
-from discord import ButtonStyle, Interaction
+import wavelink
+from discord import Interaction
 from discord._types import ClientT
-from discord.ui import Button, View
-
-from custom_dataclasses import Track
-from utils import get_info_yt
+from discord.ui import View, Button
 
 
 class GPTQuestion(discord.ui.Modal, title="GPT Question"):
@@ -55,83 +53,6 @@ class GPTQuestion(discord.ui.Modal, title="GPT Question"):
             await interaction.followup.send(embed=embed)
 
 
-class PlaylistView(View):
-    def __init__(self, interaction: discord.Interaction, player):
-        super().__init__(timeout=None)
-        self.interaction = interaction
-        self.playlist = player.queue
-        self.player = player
-        self.page_size = 5
-        self.total_pages = (len(self.playlist) + self.page_size - 1) // self.page_size
-        self.page_number = 1
-        self.update_buttons()
-        self.player = player
-        self.message: discord.InteractionMessage | None = None  # Добавляем атрибут message
-
-    def update_buttons(self):
-        self.clear_items()
-
-        empty_playlist = len(self.playlist) == 0
-
-        self.add_item(PlaylistButton(label="Назад",
-                                     custom_id="prev_page",
-                                     disabled=self.page_number == 1 or empty_playlist))
-
-        self.add_item(PlaylistButton(label="Вперед",
-                                     custom_id="next_page",
-                                     disabled=self.page_number == self.total_pages or empty_playlist))
-
-        self.add_item(CloseButton(self.interaction))
-
-    async def update_embed(self):
-        start_index = (self.page_number - 1) * self.page_size
-        end_index = start_index + self.page_size
-
-        embed = discord.Embed(title="Плейлист", color=discord.Color.green())
-
-        if self.player.current and len(self.playlist) == 0:
-            embed.description = (f"Сейчас играет: {self.player.current.title}\n"
-                                 f"Продолжительность: {self.player.current.duration_to_time()}\n"
-                                 f"Ссылка: {self.player.current.original_url}")
-            embed.add_field(name="Плейлист пуст", value="Добавьте треки в плейлист")
-            embed.set_footer(text=f"Страница {self.page_number}/{self.total_pages}")
-
-        else:
-            for index, track in enumerate(self.playlist[start_index:end_index]):
-                try:
-                    embed.add_field(
-                        name=f"{index + 1}. {track.title}",
-                        value=f"Продолжительность: {track.duration_to_time()}",
-                        inline=False
-                    )
-                except Exception as e:
-                    print(f"Error processing track {track.url}: {e}")
-
-            embed.set_footer(text=f"Страница {self.page_number}/{self.total_pages}")
-
-        if self.message:
-            await self.message.edit(embed=embed, view=self)
-
-
-class PlaylistButton(Button):
-    def __init__(self, label: str, custom_id: str, disabled: bool = False):
-        super().__init__(style=ButtonStyle.primary, label=label, custom_id=custom_id, disabled=disabled)
-
-    async def callback(self, interaction: discord.Interaction):
-        await interaction.response.defer(ephemeral=False, thinking=True)
-
-        if self.custom_id == "prev_page":
-            self.view.page_number = max(1, self.view.page_number - 1)
-        elif self.custom_id == "next_page":
-            self.view.page_number = min(self.view.total_pages, self.view.page_number + 1)
-        elif self.custom_id == "close":
-            self.view.stop()
-            await self.view.interaction.delete_original_response()
-            return
-
-        await self.view.update_embed()
-
-
 class TracebackShowButton(View):
     def __init__(self, traceback_text: str):
         super().__init__(timeout=None)
@@ -155,21 +76,21 @@ class TracebackShowButton(View):
         await interaction.delete_original_response()
 
 
-class YouTubeSearch(View):
-    def __init__(self, interaction: discord.Interaction, results: List[dict], player):
+class SearchTrack(View):
+    def __init__(self, interaction: discord.Interaction, voice_client: wavelink.Player, tracks: List[wavelink.YouTubeTrack]):
         super().__init__(timeout=None)
         self.interaction = interaction
-        self.results = results
-        self.player = player
+        self.voice_client = voice_client
+        self.tracks = tracks
+        self.update_buttons()
 
-    async def update_buttons(self):
-        for index, result in enumerate(self.results):
+    def update_buttons(self):
+        for index, track in enumerate(self.tracks):
             self.add_item(
                 SearchButton(
-                    self.interaction,
-                    f"https://youtube.com{result['url_suffix']}",
-                    self.player,
-                    index
+                    index=index,
+                    player=self.voice_client,
+                    track=track
                 )
             )
 
@@ -181,34 +102,24 @@ class YouTubeSearch(View):
 
 
 class SearchButton(Button):
-    def __init__(self, interaction: discord.Interaction, track_url: str, player, index: int):
-        super().__init__(style=ButtonStyle.blurple, label=f"Трек #{index + 1}", emoji="🎵", row=0)
-        self.interaction = interaction
-        self.track_url = track_url
+    def __init__(self, index: int, player: wavelink.Player, track: wavelink.YouTubeTrack):
+        super().__init__(
+            label=f"Трек #{index + 1}",
+            custom_id=f"search_track:{index}",
+            style=discord.ButtonStyle.primary,
+            row=0
+        )
+        self.index = index
         self.player = player
+        self.track = track
 
     async def callback(self, interaction: Interaction[ClientT]) -> Any:
-        await interaction.response.defer(ephemeral=False, thinking=True)
-        track = await get_info_yt(self.track_url)
-        self.player.add(track)
-        embed = discord.Embed(title="Добавлено в очередь", description=f"{track.title}",
-                              color=discord.Color.green())
-        embed.add_field(name="Продолжительность", value=f"{track.duration_to_time()}")
-        embed.add_field(name="Ссылка", value=f"{track.original_url}", inline=False)
-        embed.set_thumbnail(url=track.image_url)
-        if not interaction.guild.voice_client:
-            await self.player.start_play(interaction)
+        await interaction.response.defer(ephemeral=True)
+        await self.player.queue.put_wait(self.track)
+        embed = discord.Embed(title="Трек добавлен в очередь", color=discord.Color.green())
         message = await interaction.followup.send(embed=embed, wait=True)
         await asyncio.sleep(5)
         await message.delete()
+        if not self.player.is_playing():
+            await self.player.play(await self.player.queue.get_wait())
 
-
-class CloseButton(Button):
-    def __init__(self, interaction: discord.Interaction):
-        super().__init__(style=ButtonStyle.red, label="Закрыть")
-        self.interaction = interaction
-
-    async def callback(self, interaction: discord.Interaction):
-        await interaction.response.defer(ephemeral=True)
-        self.view.stop()
-        await self.view.interaction.delete_original_response()
